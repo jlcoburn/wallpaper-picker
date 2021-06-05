8a7af7608a69739cfb56607953538b7c12de6f09
Jason Coburn <jlcoburn@live.com>
2021-06-05 10:33:31 -0400

    Initial upload

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (<div id="navbar">
+    <p id="title">Wallpaper picker</p>
+    <a href="#" id="about">About</a>
+  </div>)
+}
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Searchbar() {
+  return (<div id="Searchbar">
+    <input name="search" id="search" type="text"/>
+  </div>)
+}
